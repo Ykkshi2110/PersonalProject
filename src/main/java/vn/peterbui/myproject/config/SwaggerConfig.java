ae9a8f06134eb2f04a4d481d64e6b65353ee8d77
Ykkshi2110 <quocbui2110@gmail.com>
2024-09-22 21:37:47 +0700

    config basic Swagger 3.0

---
+++ src/main/java/vn/peterbui/myproject/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package vn.peterbui.myproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenApi() {
+        final String securitySchemeName = "Auth Type";
+        return new OpenAPI()
+                .info(new Info().title("Tan Xuan Food Api")
+                        .version("v1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
